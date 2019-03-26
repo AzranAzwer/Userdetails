e45c8e9fb8c4310f312c0cf87d552d763260979e
azran-azwer <azranmam@gmail.com>
2019-03-27 00:53:14 +0530

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,18 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import { NavComponent } from "./nav/nav.component";
+import { AddPersonComponent } from "./add-person/add-person.component";
+import { HomeComponent } from "./home/home.component";
+
+import { HttpClientModule } from "@angular/common/http";
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent, NavComponent, AddPersonComponent, HomeComponent],
+  imports: [BrowserModule, AppRoutingModule, HttpClientModule],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
---
+++ src/app/home/home.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from "@angular/core";
+import { DataService } from "../data.service";
+import { Observable } from "rxjs";
+
+@Component({
+  selector: "app-home",
+  templateUrl: "./home.component.html",
+  styleUrls: ["./home.component.scss"]
+})
+export class HomeComponent implements OnInit {
+  home$: Object;
+
+  constructor(private data: DataService) {}
+
+  ngOnInit() {
+    this.data.getUsers().subscribe(data => (this.home$ = data));
+  }
+}
